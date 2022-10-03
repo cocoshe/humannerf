8ce674ac7575e030cc09dbd2ba51548d3d1d091c
cocoshe <1228759711@qq.com>
2022-10-04 02:16:43 +0800

    feat(convert): ROMP output to SMPL poses sequence

---
+++ scripts/novel_pose_generator.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import sys
+
+import numpy as np
+
+
+
+# cam (1, 3)
+# global_orient (1, 3)
+# body_pose (1, 69)
+# smpl_betas (1, 10)
+# smpl_thetas (1, 72)
+# center_preds (1, 2)
+# center_confs (1, 1)
+# cam_trans (1, 3)
+# verts (1, 6890, 3)
+# joints (1, 71, 3)
+# pj2d_org (1, 71, 2)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', type=str, default='./video_results.npz')
+parser.add_argument('-o', '--output', type=str, default='./novel_poses.npy')
+
+args = parser.parse_args(sys.argv[1:])
+
+npz_path = args.path
+# if os.path.exists('video_results.npz'):
+#     print('find video_results.npz')
+# else: print('please input the ')
+if os.path.exists(npz_path):
+    print('find video_results.npz in ', npz_path)
+else:
+    print(npz_path, ' not found')
+
+
+
+# data = np.load('video_results (1).npz', allow_pickle=True)  # kun part1
+# data = np.load('video_results (2).npz', allow_pickle=True)  # girl dance20s
+# data = np.load('video_results (3).npz', allow_pickle=True)  # smooth kun part1
+data = np.load('video_results (4).npz', allow_pickle=True)  # aini_all
+data_dict = data['results'][()]
+# print(data['results']())
+pose_output = []
+n_frames = len(data_dict.keys())
+print('n_frames ', n_frames)
+for i, (k, v) in enumerate(data_dict.items()):
+    # print(v['global_orient'][0].dtype)
+    # v['global_orient'][0] += 180. * np.pi / 180.
+    pose_output.append(np.concatenate([v['global_orient'][0], v['body_pose'][0]], axis=0))
+
+# np.save('novel_poses.npy', pose_output)
+np.save(args.output, pose_output)
+print('save novel_poses.npy in ', args.output)
+
+# novel = np.load('novel_poses.npy')
+# print(novel.shape)
+
+# print(pose_output[0])
